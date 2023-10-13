4ab415486a0a6214e98fcf12ad514475326ed32f
RENATO DOUGLAS <83033433+renatdoug@users.noreply.github.com>
2023-10-13 02:07:16 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -20,22 +20,21 @@ def morse_scale_score(history_of_falls, secondary_diagnosis, ambulatory_aid, IV_
         score += 15
     return score
 
-def evaruci_scale_score(consciousness, hemodynamics, respiratory, mobility, skin_condition, other_factors, length_of_stay):
-    score = consciousness + hemodynamics + respiratory + mobility + skin_condition + other_factors
-    score += 0.5 * min(length_of_stay, 4)  # Adiciona 0,5 ponto para cada semana de internação, até 2 pontos
+def evaruci_scale_score(consciousness, hemodynamics, respiratory, mobility, other_factors, length_of_stay):
+    score = consciousness + hemodynamics + respiratory + mobility + other_factors
+    score += min(length_of_stay * 0.5, 2)  # Adiciona 0,5 ponto para cada semana de internação, até 2 pontos
     return score
 
 def risk_category(score):
-    if score >= 45:
-        return "Alto Risco"
-    elif score >= 25:
-        return "Moderado Risco"
+    if score >= 51:
+        return "Risco muito elevado"
+    elif score >= 41:
+        return "Risco elevado"
     else:
-        return "Baixo Risco"
+        return "Risco médio"
 
 st.title("Avaliação de Risco de Lesão por Pressão em Cuidados Intensivos")
 st.subheader("Escala de Morse")
-
 history_of_falls = st.radio("Histórico de Quedas", ("Não", "Sim"))
 secondary_diagnosis = st.radio("Diagnóstico Secundário", ("Não", "Sim"))
 ambulatory_aid = st.radio("Auxílio na Deambulação", ("Nenhum/Acamado/Auxiliado por Profissional de Saúde", "Muletas/Bengala/Andador", "Mobilidade/Parede"))
@@ -43,23 +42,41 @@ IV_heparin = st.radio("Presença de Soro Intravenoso (IV) ou Heparina", ("Não",
 mobility = st.radio("Marcha", ("Normal/Sem deambulação, Acamado, Cadeira de Rodas", "Fraca", "Comprometida/Cambaleante"))
 mental_status = st.radio("Estado Mental", ("Orientado/capaz quanto a sua capacidade/limitação", "Superestina capacidade/Esquece limitações"))
 
-morse_total_score = morse_scale_score(history_of_falls, secondary_diagnosis, ambulatory_aid, IV_heparin, mobility, mental_status)
+if st.button("Calcular Pontuação da Escala de Morse"):
+    morse_total_score = morse_scale_score(history_of_falls, secondary_diagnosis, ambulatory_aid, IV_heparin, mobility, mental_status)
+    morse_risk = risk_category(morse_total_score)
+    st.success(f"Pontuação total da Escala de Morse: {morse_total_score} - Tipo de Risco: {morse_risk}")
 
 st.subheader("Escala EVARUCI")
-consciousness = st.slider("Consciência (1-4)", 1, 4, 1)
-hemodynamics = st.slider("Hemodinâmica (1-4)", 1, 4, 1)
-respiratory = st.slider("Respiratório (1-4)", 1, 4, 1)
-mobility_evaruci = st.slider("Mobilidade (1-4)", 1, 4, 1)
-skin_condition = st.slider("Estado da Pele (1-4)", 1, 4, 1)
-other_factors = st.slider("Outros Fatores (1-4)", 1, 4, 1)
+consciousness = st.checkbox("Consciência: Consciente (1 ponto)")
+consciousness += st.checkbox("Consciência: Colaborativo (2 pontos)") * 2
+consciousness += st.checkbox("Consciência: Reativo (3 pontos)") * 3
+consciousness += st.checkbox("Consciência: Arreativo (4 pontos)") * 4
+
+hemodynamics = st.checkbox("Hemodinâmica: Sem suporte (1 ponto)")
+hemodynamics += st.checkbox("Hemodinâmica: Com expansão (2 pontos)") * 2
+hemodynamics += st.checkbox("Hemodinâmica: Com dopamina ou dobutamina (3 pontos)") * 3
+hemodynamics += st.checkbox("Hemodinâmica: Com adrenalina ou noradrenalina (4 pontos)") * 4
+
+respiratory = st.checkbox("Respiratório: Com baixa necessidade de O2 (1 ponto)")
+respiratory += st.checkbox("Respiratório: Com alta necessidade de O2 (2 pontos)") * 2
+respiratory += st.checkbox("Respiratório: Com suporte respiratório (3 pontos)") * 3
+respiratory += st.checkbox("Respiratório: Com ventilação mecânica invasiva (4 pontos)") * 4
+
+mobility_evaruci = st.checkbox("Mobilidade: Independente (1 ponto)")
+mobility_evaruci += st.checkbox("Mobilidade: Dependente mas se movimenta (2 pontos)") * 2
+mobility_evaruci += st.checkbox("Mobilidade: Pouca mobilidade (3 pontos)") * 3
+mobility_evaruci += st.checkbox("Mobilidade: Sem mobilidade (4 pontos)") * 4
+
+other_factors = st.checkbox("Outros: Temperatura ≥38˚C (1 ponto)")
+other_factors += st.checkbox("Outros: Saturação de O2 < 90% (1 ponto)")
+other_factors += st.checkbox("Outros: PA sistólica < 100 mmHg (1 ponto)")
+other_factors += st.checkbox("Outros: Estado da pele (1 ponto)")
+other_factors += st.checkbox("Outros: Paciente em prona (1 ponto)")
+
 length_of_stay = st.slider("Semanas de Internação na UTI (0-4)", 0, 4, 0)
 
-evaruci_total_score = evaruci_scale_score(consciousness, hemodynamics, respiratory, mobility_evaruci, skin_condition, other_factors, length_of_stay)
-
-if st.button("Calcular Pontuação"):
-    st.success(f"Pontuação total da Escala de Morse: {morse_total_score}")
-    st.success(f"Pontuação total da Escala EVARUCI: {evaruci_total_score}")
-    morse_risk = risk_category(morse_total_score)
+if st.button("Calcular Pontuação da Escala EVARUCI"):
+    evaruci_total_score = evaruci_scale_score(consciousness, hemodynamics, respiratory, mobility_evaruci, other_factors, length_of_stay)
     evaruci_risk = risk_category(evaruci_total_score)
-    st.success(f"Tipo de Risco (Escala de Morse): {morse_risk}")
-    st.success(f"Tipo de Risco (Escala EVARUCI): {evaruci_risk}")
+    st.success(f"Pontuação total da Escala EVARUCI: {evaruci_total_score} - Tipo de Risco: {evaruci_risk}")
